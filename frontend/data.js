229473f0027a881920685e5bfc30af2a97b0bdf6
anyas13 <65509295+anyas13@users.noreply.github.com>
2021-10-01 23:42:05 -0700

    Create data.js

---
+++ frontend/data.js
@@ -0,0 +1,27 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "long sleeve tshirt",
+      image: "/images/p1.jpg",
+      price: "120",
+      countInStock: 10,
+    },
+    {
+      _id: "2",
+      name: "black bow blouse",
+      image: "/images/p2.jpg",
+      price: "150",
+      countInStock: 20,
+    },
+    {
+      _id: "3",
+      name: "au revoir sweater",
+      image: "/images/p3.jpg",
+      price: "210",
+      countInStock: 30,
+    },
+  ],
+};
+
+export default data;
